4ace957c81a1222eb9b34780db196a9f47da3306
161250095 <161250095@smail.nju.edu.cn>
2018-09-11 01:28:43 +0800

    loan all controller 修改

--- src/main/java/com/ipnet/controller/loancontroller/LoanAllController.java
+++ src/main/java/com/ipnet/controller/loancontroller/LoanAllController.java
@@ -21,39 +21,56 @@ public class LoanAllController {
 
     /**
      * 获取该合同的所有方信息
+     *
      * @param loanID 贷款号
      * @return 0-专利持有人 1-金融机构 2-保险公司 3-评估机构 4-政府
      */
     @RequestMapping("/getContract")
     @ResponseBody
-    public ArrayList<String> getContract(String loanID){
+    public ArrayList<String> getContract(String loanID) {
         return loanBLService.getContract(loanID);
     }
 
     /**
      * 存取该合同的用户是否同意并签署最终合同
+     *
      * @param loanID 贷款号
-     * @param url 上传签字的url
+     * @param url    上传签字的url
      * @param userid 用户
      * @param ifPass 是否同意
      * @return
      */
     @RequestMapping("/ifContract")
     @ResponseBody
-    public ResultMessage ifContract(String loanID,String url, String userid, Boolean ifPass){
+    public ResultMessage ifContract(String loanID, String url, String userid, Boolean ifPass) {
         return null;
-      //  return loanBLService.ifContract(loanID,userid,ifPass);
+        //  return loanBLService.ifContract(loanID,userid,ifPass);
+    }
+
+    /**
+     * 存取合同url
+     * @param loanID 贷款ID
+     * @param userid 用户ID
+     * @param gov 如果用户为专利持有人，此参数有效，0为知识产权局，1为财政局
+     * @param url url
+     * @return
+     */
+    @RequestMapping("/saveContractURL")
+    @ResponseBody
+    public ResultMessage saveContractURL(String loanID, String userid, int gov, String url) {
+        return null;
     }
 
     /**
      * 获取合同照片的url
+     *
+     *
      * @param loanID 贷款号
-     * @param userid 用户ID
-     * @return
+     * @return 0-知识产权局 1-财政局 2-金融机构 3-保险公司 4-评估机构, 如果没有该项返回""
      */
-    @RequestMapping("/ifContract")
+    @RequestMapping("/getContractURL")
     @ResponseBody
-    public String getContractURL(String loanID, String userid){
+    public ArrayList<String> getContractURL(String loanID) {
         return null;
     }
 }
