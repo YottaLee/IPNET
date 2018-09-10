3e36222aed936114ef950eb2ffc29c9c3beed3e1
161250029 <161250029@smail.nju.edu.cn>
2018-09-10 18:01:47 +0800

    //所属专利池

--- src/main/java/com/ipnet/blservice/apiservice/AccountService.java
+++ src/main/java/com/ipnet/blservice/apiservice/AccountService.java
@@ -4,6 +4,6 @@ import java.io.IOException;
 
 public interface AccountService {
         public String GetAccounts(String username ,String password) throws IOException;
-        public String GetAccountDetails(String username ,String password) throws IOException;
-        public String GetTransaction(String username ,String password) throws IOException;
+        public String GetAccountDetails(String username ,String password , String accountId) throws IOException;
+        public String GetTransaction(String username ,String password , String accountId) throws IOException;
 }
