785a249127f22a9459aef1911ea86006f06a25fc
161250095 <161250095@smail.nju.edu.cn>
2018-09-10 10:45:03 +0800

    controller url

--- src/main/java/com/ipnet/controller/evaluationcontroller/EvaluationController.java
+++ src/main/java/com/ipnet/controller/evaluationcontroller/EvaluationController.java
@@ -12,13 +12,26 @@ public class EvaluationController {
 
     /**
      * 提交专利评估报告
-     * @patentID 专利号
-     * @evaluation 经济评估价值
+     * @param patentID 专利号
+     * @param url 上传评估报告的文件路径
+     * @param evaluation 经济评估价值
      * @return
      */
     @RequestMapping("/submitReport")
     @ResponseBody
-    public ResultMessage submitReport(String patentID, int evaluation) {
+    public ResultMessage submitReport(String patentID,String url, int evaluation) {
+        return null;
+    }
+
+    /**
+     * 申请评估
+     * @param patentID 专利号
+     * @param url 申请评估文件的路径
+     * @return
+     */
+    @RequestMapping("/apply")
+    @ResponseBody
+    public ResultMessage applyEvaluation(String patentID, String url) {
         return null;
     }
 }
