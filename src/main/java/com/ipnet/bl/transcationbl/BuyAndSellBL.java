a233a2f87bffccaf3d65dbf48623d80a77c5ec11
liangnan1215 <522416667@qq.com>
2018-09-15 17:25:36 +0800

    temp contract model

--- src/main/java/com/ipnet/bl/transcationbl/BuyAndSellBL.java
+++ src/main/java/com/ipnet/bl/transcationbl/BuyAndSellBL.java
@@ -6,6 +6,7 @@ import com.ipnet.blservice.transcationservice.TranscationBlService;
 import com.ipnet.dao.CompanyUserDao;
 import com.ipnet.dao.PersonalUserDao;
 import com.ipnet.entity.contract.Contract;
+import com.ipnet.enums.ContractType;
 import com.ipnet.enums.ResultMessage;
 import com.ipnet.enums.TranscationType;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,22 +25,50 @@ public class BuyAndSellBL implements TranscationBlService {
 
 
     @Override
-    public ResultMessage draftContract(Contract contract, TranscationType transcationType) {
-        return null;
+    public ResultMessage draftContract(Contract contract, TranscationType transcationType,ContractType contractType) {
+        switch(contractType){
+            case Loan:
+            case Agent:
+            case Permit:
+            case Breakup:
+            case Transfer:
+        }
+        return ResultMessage.Fail;
     }
 
     @Override
-    public ResultMessage confirmContract(String contract_id) {
-        return null;
+    public ResultMessage confirmContract(String contract_id, ContractType contractType) {
+        switch(contractType){
+            case Loan:
+            case Agent:
+            case Permit:
+            case Breakup:
+            case Transfer:
+        }
+        return ResultMessage.Fail;
     }
 
     @Override
-    public ResultMessage payContract(String contract_id, double rmb) {
-        return null;
+    public ResultMessage payContract(String contract_id, double rmb, ContractType contractType) {
+
+        switch(contractType){
+            case Loan:
+            case Agent:
+            case Permit:
+            case Breakup:
+            case Transfer:
+        }return ResultMessage.Fail;
     }
 
     @Override
-    public ResultMessage completeTranscation(String contract_id, TranscationType transcationType) {
-        return null;
+    public ResultMessage completeTranscation(String contract_id, TranscationType transcationType, ContractType contractType) {
+        switch(contractType){
+            case Loan:
+            case Agent:
+            case Permit:
+            case Breakup:
+            case Transfer:
+        }
+        return ResultMessage.Fail;
     }
 }
--- src/main/java/com/ipnet/entity/contract/Contract.java
+++ src/main/java/com/ipnet/entity/contract/Contract.java
@@ -4,12 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.util.Date;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class  Contract {
+    @Id
     private String contract_id;
     private String partyA;
     private String partyB;
