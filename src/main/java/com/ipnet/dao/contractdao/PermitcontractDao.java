b731bb710478fa29553d1042d78ad8c86779c159
liangnan1215 <522416667@qq.com>
2018-09-15 10:39:46 +0800

    update contract

---
+++ src/main/java/com/ipnet/dao/contractdao/PermitcontractDao.java
@@ -0,0 +1,13 @@
+package com.ipnet.dao.contractdao;
+
+import com.ipnet.entity.contract.AgentContract;
+import com.ipnet.entity.contract.PermitContract;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.Table;
+
+@Repository
+@Table(name = "permit_contract")
+public interface PermitcontractDao extends JpaRepository<PermitContract, String> {
+}
