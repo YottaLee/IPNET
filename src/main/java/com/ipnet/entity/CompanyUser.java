9d53c7e967c70f91c62c9089b72d3d180b8b3990
zp2946062178 <2799712178@qq.com>
2018-09-16 15:41:01 +0800

    commit

--- src/main/java/com/ipnet/entity/CompanyUser.java
+++ src/main/java/com/ipnet/entity/CompanyUser.java
@@ -7,11 +7,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import java.util.ArrayList;
+import javax.persistence.*;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Data
@@ -46,248 +44,16 @@ public class CompanyUser {
     private String creditCode;//法人信用代码
     private double money;//人民币
     private int points;//积分
-    //@ElementCollection(targetClass = String.class,fetch = FetchType.LAZY)
-    private ArrayList<String> bank_accounts;//银行账号
+    @ElementCollection(targetClass = String.class,fetch = FetchType.LAZY)
+    private List<String> bank_accounts;//银行账号
     private String pay_code;
     private String password;//密码
-    //@ElementCollection(targetClass = String.class,fetch = FetchType.LAZY)
-    private ArrayList<Identity> identities;//身份认证
+    @ElementCollection(targetClass = String.class,fetch = FetchType.LAZY)
+    private List<Identity> identities;//身份认证
 
     private String description;//自我描述
     private Date registerTime;//注册日期
 
     private boolean isActive;//用户的激活状态
     private String activeCode;//用户的激活码
-
-    /*public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getTel() {
-        return tel;
-    }
-
-    public void setTel(String tel) {
-        this.tel = tel;
-    }
-
-    public boolean isVerified() {
-        return verified;
-    }
-
-    public void setVerified(boolean verified) {
-        this.verified = verified;
-    }
-
-    public String getImage() {
-        return image;
-    }
-
-    public void setImage(String image) {
-        this.image = image;
-    }
-
-    public String getLicence() {
-        return licence;
-    }
-
-    public void setLicence(String licence) {
-        this.licence = licence;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Role getRole() {
-        return role;
-    }
-
-    public void setRole(Role role) {
-        this.role = role;
-    }
-
-    public String getRepresentative() {
-        return representative;
-    }
-
-    public void setRepresentative(String representative) {
-        this.representative = representative;
-    }
-
-    public String getPersonPhoto() {
-        return personPhoto;
-    }
-
-    public void setPersonPhoto(String personPhoto) {
-        this.personPhoto = personPhoto;
-    }
-
-    public double getFund() {
-        return fund;
-    }
-
-    public void setFund(double fund) {
-        this.fund = fund;
-    }
-
-    public Date getEstablishDate() {
-        return establishDate;
-    }
-
-    public void setEstablishDate(Date establishDate) {
-        this.establishDate = establishDate;
-    }
-
-    public String getBusTerm() {
-        return busTerm;
-    }
-
-    public void setBusTerm(String busTerm) {
-        this.busTerm = busTerm;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getStatement() {
-        return statement;
-    }
-
-    public void setStatement(String statement) {
-        this.statement = statement;
-    }
-
-    public CompanyType getType() {
-        return type;
-    }
-
-    public void setType(CompanyType type) {
-        this.type = type;
-    }
-
-    public String getField() {
-        return field;
-    }
-
-    public void setField(String field) {
-        this.field = field;
-    }
-
-    public String getWebsite() {
-        return website;
-    }
-
-    public void setWebsite(String website) {
-        this.website = website;
-    }
-
-    public String getBusinessNum() {
-        return businessNum;
-    }
-
-    public void setBusinessNum(String businessNum) {
-        this.businessNum = businessNum;
-    }
-
-    public String getCreditCode() {
-        return creditCode;
-    }
-
-    public void setCreditCode(String creditCode) {
-        this.creditCode = creditCode;
-    }
-
-    public double getMoney() {
-        return money;
-    }
-
-    public void setMoney(double money) {
-        this.money = money;
-    }
-
-    public int getPoints() {
-        return points;
-    }
-
-    public void setPoints(int points) {
-        this.points = points;
-    }
-
-    public ArrayList<String> getBank_accounts() {
-        return bank_accounts;
-    }
-
-    public void setBank_accounts(ArrayList<String> bank_accounts) {
-        this.bank_accounts = bank_accounts;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public ArrayList<Identity> getIdentities() {
-        return identities;
-    }
-
-    public void setIdentities(ArrayList<Identity> identities) {
-        this.identities = identities;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getRegisterTime() {
-        return registerTime;
-    }
-
-    public void setRegisterTime(String registerTime) {
-        this.registerTime = registerTime;
-    }
-
-    public boolean isActive() {
-        return isActive;
-    }
-
-    public void setActive(boolean active) {
-        isActive = active;
-    }
-
-    public String getActiveCode() {
-        return activeCode;
-    }
-
-    public void setActiveCode(String activeCode) {
-        this.activeCode = activeCode;
-    }*/
 }
