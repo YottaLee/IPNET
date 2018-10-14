d7330f42d9a060bb0bc03655bbf9b08338c544e0
liangnan1215 <522416667@qq.com>
2018-10-14 23:21:50 +0800

    update

--- src/main/java/com/ipnet/entity/contract/Account.java
+++ src/main/java/com/ipnet/entity/contract/Account.java
@@ -5,9 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 
 @Entity
@@ -17,6 +15,7 @@ import javax.persistence.Table;
 @Table(name = "account")
 public class Account {
       @Id
+      @GeneratedValue(strategy = GenerationType.AUTO)
       private String acountId;       //银行卡号
       private String userId;
       private double balance;       //账户余额;
