d9fc184249a1fcef8227c78aa6bb3c10302ca68b
liangnan1215 <522416667@qq.com>
2018-10-16 19:57:46 +0800

    udpate

--- src/main/java/com/ipnet/vo/CreditCard.java
+++ src/main/java/com/ipnet/vo/CreditCard.java
@@ -1,11 +1,17 @@
 package com.ipnet.vo;
 
+import lombok.*;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
 public class CreditCard {
     String cardId;
     String bank;
-
-    public CreditCard(String cardId, String bank) {
-        this.cardId = cardId;
-        this.bank = bank;
-    }
+    
 }
